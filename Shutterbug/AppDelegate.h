a4140d61cfde8fc158ff8fefea891ae503a7485a
princeli <princeli@live.cn>
2015-12-23 17:25:52 +0800

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by ly on 15/12/23.
+//  Copyright © 2015年 princeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by ly on 15/12/23.
+//  Copyright © 2015年 princeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
